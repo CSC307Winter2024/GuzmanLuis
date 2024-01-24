a6e3ab6df520c502675f43582f44b299d69b826e
Luis Guzman <lguzma04@calpoly.edu>
2024-01-23 19:53:00 -0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+        <h1>My Resume</h1>
+            <p>Name: [Luis Guzman]</p>
+            <p>Address: [2209 N Prospect]</p>
+            <p>Phone: [5599403790]</p>
+            <p>Email: [lguzma04@calpoly.edu]</p>
+            <p>LinkedIn: [https://www.linkedin.com/in/luis-guzman-/]</p>
+    </main>
+  );
+}
